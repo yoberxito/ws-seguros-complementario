09dbbef9894195ed77df520ca86db1e34d70e9ea
yober cieza <gctic.sgsass12@essalud.gob.pe>
2026-07-13 12:44:19 -0500

    version inicial del proyecto

---
+++ src/main/java/essalud/gob/pe/wsseguroscomplementario/WsSegurosComplementarioApplication.java
@@ -0,0 +1,13 @@
+package essalud.gob.pe.wsseguroscomplementario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsSegurosComplementarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsSegurosComplementarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/essalud/gob/pe/wsseguroscomplementario/WsSegurosComplementarioApplicationTests.java
@@ -0,0 +1,13 @@
+package essalud.gob.pe.wsseguroscomplementario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsSegurosComplementarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
